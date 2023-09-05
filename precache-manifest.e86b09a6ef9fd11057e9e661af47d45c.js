9058f0dfb591354d2be98ab60e332548a50fdfa1
Ivan Akulenka <ivan.akulenka@pearson.com>
2023-09-05 16:40:48 +0300

    aws cognito test app

---
+++ precache-manifest.e86b09a6ef9fd11057e9e661af47d45c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69c53049d0e9a55122c4333ca9bf5e85",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be9a51500c83cec3c3fd",
+    "url": "/static/css/2.68dd0589.chunk.css"
+  },
+  {
+    "revision": "c98fccb96b4fdc18ee35",
+    "url": "/static/css/main.faa2f4be.chunk.css"
+  },
+  {
+    "revision": "be9a51500c83cec3c3fd",
+    "url": "/static/js/2.563a208f.chunk.js"
+  },
+  {
+    "revision": "862d42b3263b5420588128ff7b8aae1d",
+    "url": "/static/js/2.563a208f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c98fccb96b4fdc18ee35",
+    "url": "/static/js/main.5612cf14.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.5612cf14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c45c39f353c95180c79",
+    "url": "/static/js/runtime-main.08dcfcde.js"
+  }
+]);
